8c585f1306e1f73afcfe8af149c9273ce50ee8a1
Christine <yost90@gmail.com>
2021-07-09 17:28:19 -0600

    Complete exercise 8

--- src/__tests__/exercise/08.js
+++ src/__tests__/exercise/08.js
@@ -1,22 +1,95 @@
 // testing custom hooks
 // http://localhost:3000/counter-hook
 
-import * as React from 'react'
-import {render, screen} from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
+import {renderHook, act} from '@testing-library/react-hooks'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+// const TestComponent = (props = {}) => {
+//   const {count, increment, decrement} = useCounter(props)
 
-test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+//   return (
+//     <div>
+//       <p>Count: {count}</p>
+//       <button onClick={increment}>Increment</button>
+//       <button onClick={decrement}>Decrement</button>
+//     </div>
+//   )
+// }
+
+// test('exposes the count and increment/decrement functions', () => {
+//   render(<TestComponent />)
+//   const count = screen.getByText(/count/i)
+//   const incrementButton = screen.getByRole('button', {name: /increment/i})
+//   const decrementButton = screen.getByRole('button', {name: /decrement/i})
+
+//   expect(count).toHaveTextContent('Count: 0')
+
+//   userEvent.click(incrementButton)
+//   expect(count).toHaveTextContent('Count: 1')
+
+//   userEvent.click(decrementButton)
+//   expect(count).toHaveTextContent('Count: 0')
+// })
+
+// test('uses initialCount configuration', () => {
+//   render(<TestComponent initialCount={10} />)
+//   const count = screen.getByText(/count/i)
+//   expect(count).toHaveTextContent('Count: 10')
+// })
+
+// test('uses step configuration', () => {
+//   render(<TestComponent step={2} />)
+//   const count = screen.getByText(/count/i)
+//   const incrementButton = screen.getByRole('button', {name: /increment/i})
+//   const decrementButton = screen.getByRole('button', {name: /decrement/i})
+
+//   expect(count).toHaveTextContent('Count: 0')
+
+//   userEvent.click(incrementButton)
+//   expect(count).toHaveTextContent('Count: 2')
+
+//   userEvent.click(decrementButton)
+//   expect(count).toHaveTextContent('Count: 0')
+// })
+
+test('test with a fake component', () => {
+  const {result} = renderHook(useCounter)
+
+  expect(result.current.count).toEqual(0)
+  act(() => result.current.decrement())
+  expect(result.current.count).toEqual(-1)
+  act(() => result.current.increment())
+  expect(result.current.count).toEqual(0)
 })
 
-/* eslint no-unused-vars:0 */
+test('allows customization of the initial count', () => {
+  const {result} = renderHook(useCounter, {initialProps: {initialCount: 10}})
+
+  expect(result.current.count).toEqual(10)
+  act(() => result.current.decrement())
+  expect(result.current.count).toEqual(9)
+  act(() => result.current.increment())
+  expect(result.current.count).toEqual(10)
+})
+
+test('allows customization of the step', () => {
+  const {result} = renderHook(useCounter, {initialProps: {step: 4}})
+
+  expect(result.current.count).toEqual(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toEqual(4)
+  act(() => result.current.decrement())
+  expect(result.current.count).toEqual(0)
+})
+
+test('step can be changed', () => {
+  const {result, rerender} = renderHook(useCounter, {initialProps: {step: 4}})
+
+  expect(result.current.count).toEqual(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toEqual(4)
+
+  rerender({step: 2})
+  act(() => result.current.decrement())
+  expect(result.current.count).toEqual(2)
+})
